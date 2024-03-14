cd56984a75b45bde40882fbd7acab43e1ebf6fec
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-03-14 14:25:00 -0300

    fonts

--- Pomodoro/DesignSystem/Fonts.swift
+++ Pomodoro/DesignSystem/Fonts.swift
@@ -6,10 +6,106 @@
 //
 
 import Foundation
+import SwiftUI
+
 extension Font {
-    static func largeTitleFont() -> Font {
-        return Font.custom("", size: 120)
+    //Noto Sans
+    
+    static func largeTitle() -> Font {
+        return Font.custom("Noto Sans", size: 120)
+            .weight(.black)
+    }
+    
+    static func title1() -> Font {
+        return Font.custom("Noto Sans", size: 28)
+            .weight(.heavy)
+    }
+    
+    static func title2() -> Font {
+        return Font.custom("Noto Sans", size: 22)
             .weight(.bold)
+    }
+    
+    static func title2Heavy() -> Font {
+        return Font.custom("Noto Sans", size: 28)
+            .weight(.black)
+    }
+    
+    static func title3() -> Font {
+        return Font.custom("Noto Sans", size: 20)
+            .weight(.bold)
+    }
+    
+    static func headline() -> Font {
+        return Font.custom("Noto Sans", size: 24)
+            .weight(.semibold)
+    }
+    
+    static func heading() -> Font {
+        return Font.custom("Noto Sans", size: 20)
+            .weight(.semibold)
+    }
+    
+    static func body() -> Font {
+        return Font.custom("Noto Sans", size: 17)
+            .weight(.regular)
+    }
+    
+    static func bodyBody() -> Font {
+        return Font.custom("Noto Sans", size: 17)
+            .weight(.bold)
+    }
+    
+    static func callout() -> Font {
+        return Font.custom("Noto Sans", size: 16)
+            .weight(.regular)
+    }
+    
+    static func subhead() -> Font {
+        return Font.custom("Noto Sans", size: 15)
+            .weight(.regular)
+    }
+    
+    static func footnote() -> Font {
+        return Font.custom("Noto Sans", size: 13)
+            .weight(.regular)
+    }
+    
+    static func tag() -> Font {
+        return Font.custom("Noto Sans", size: 17)
+            .weight(.semibold)
+    }
+
+    static func notificationTitle() -> Font {
+        return Font.custom("Noto Sans", size: 15)
+            .weight(.semibold)
+    }
+    
+    //Londrina
+    
+    static func timerButton() -> Font {
+        return Font.custom("Londrina Solid", size: 24)
+            .weight(.light)
+    }
+    
+    static func stats() -> Font {
+        return Font.custom("Londrina Solid", size: 50)
+            .weight(.black)
+    }
+
+    static func timer() -> Font {
+        return Font.custom("Londrina Solid", size: 120)
+            .weight(.regular)
+    }
+    
+    static func smallTimer() -> Font {
+        return Font.custom("Londrina Solid", size: 80)
+            .weight(.regular)
+    }
+    
+    static func numberSlider() -> Font {
+        return Font.custom("Londrina Solid", size: 25)
+            .weight(.black)
     }
     
 }
